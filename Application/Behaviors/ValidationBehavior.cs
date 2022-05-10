d573e770eff269c6474c86ef01a31330453de143
Alberto Paulo <apaulo@vmware.com>
2022-05-10 12:02:55 +0200

    06-Manejando Excepciones Via Pipeline

---
+++ Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Behaviors
+{
+    // this class or pipeline will allow us to manage our own exceptions of our business rule
+    // this is a Middleware
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new FluentValidation.ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Any())
+                {
+                    throw new Exceptions.ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}

609c7a39183120346c98a6a49978aa4f8a9ac1e6
Alberto Paulo <apaulo@vmware.com>
2022-05-10 13:28:39 +0200

    08 - Repository Pattern & Persistencia

--- Application/Features/Customers/Commands/CreateCustomerCommand/CreateCustomerCommand.cs
+++ Application/Features/Customers/Commands/CreateCustomerCommand/CreateCustomerCommand.cs
@@ -12,4 +12,12 @@ namespace Application.Features.Customers.Commands.CreateCustomerCommand
         public string? Email { get; set; }
         public string? Address { get; set; }
     }
+
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, Response<int>>
+    {
+        public async Task<Response<int>> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
---
+++ Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Common;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IDateTimeService _dateTime;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IDateTimeService dateTime) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            _dateTime = dateTime;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+        // Modifying the SaveChangeAsync method in order to save the Created and LastModified date each time we invoke it
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableBaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = _dateTime.NowUtc;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModified = _dateTime.NowUtc;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}

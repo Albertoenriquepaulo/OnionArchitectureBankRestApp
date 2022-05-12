d212618f560e308fe3bd386a9033ea09b598ee68
Alberto Paulo <apaulo@vmware.com>
2022-05-12 10:53:26 +0200

    12 - Versionando y Creando Controlador Cliente

---
+++ Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Customers.Commands.CreateCustomerCommand;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            #region Commands
+            CreateMap<CreateCustomerCommand, Customer>();
+            #endregion
+        }
+    }
+}
---
+++ WebAPI/Controllers/v1/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Customers.Commands.CreateCustomerCommand;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class CustomersController : BaseApiController
+    {
+        //POST api/controller
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateCustomerCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
